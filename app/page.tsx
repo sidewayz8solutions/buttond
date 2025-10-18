542794d63d35beff13c3ba4d6d118107849f1f83
Benjamin Shirley <sidewayzaitsolutions@outlook.com>
2025-10-18 00:17:39 -0500

    Copy: fix typos/grammar in Püper card description (bathroom, Yelp of public restrooms, allows people, 5-toilet; add final period).

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
   const projects = [
     {
       name: "Püper",
-      description: "No more wondering if you will find a clean batroom in your travels. The yelp of public restroom is here. Püper allows the people to rate restrooms with our 5 toilet rating system. You can finally travel with peace of mind",
+      description: "No more wondering if you will find a clean bathroom in your travels. The Yelp of public restrooms is here. Püper allows people to rate restrooms with our 5-toilet rating system. You can finally travel with peace of mind.",
       imageSrc: "/puper.png"
     },
     {
